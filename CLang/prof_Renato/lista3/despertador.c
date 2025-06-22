3f01dd9adf16a61c8a89820d8b9b679742e7cffc
claudio-py <claudio.3777@gmail.com>
2025-06-22 20:44:45 -0300

    updates

--- CLang/prof_Renato/lista3/despertador.c
+++ CLang/prof_Renato/lista3/despertador.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 int main() {
-    int hora, minuto;
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
+  int hora, minuto;
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
 
-    printf("Digite a hora do despertador (HH MM): ");
-    scanf("%d %d", &hora, &minuto);
-    printf("Otimo, despertador programado para %02d:%02d\n", hora, minuto);
+  printf("Digite a hora do despertador: \n");
+  scanf(" %d", &hora);
+  printf("Digite o minuto: \n");
+  scanf(" %d", &minuto);
+  printf("Otimo, despertador programado para %02d:%02d\n", hora, minuto);
 
-    while (1) {
-	t = time(NULL);
-	tm = *localtime(&t);
+  while (1) {
+    t = time(NULL);
+    tm = *localtime(&t);
 
-	if (tm.tm_hour == hora && tm.tm_min == minuto) {
-	    printf("Despertador tocando! Hora: %02d:%02d\n", tm.tm_hour, tm.tm_min);
-	    break;
-	}
-
-	sleep(1); // Espera 1 segundo antes de verificar novamente
+    if (tm.tm_hour == hora && tm.tm_min == minuto) {
+      printf("Despertador tocando! Hora: %02d:%02d\n", tm.tm_hour, tm.tm_min);
+      break;
     }
 
-    return 0;
+    sleep(1); // Espera 1 segundo antes de verificar novamente
+  }
+
+  return 0;
 }
