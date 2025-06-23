5f0316e21b7338cf5d40acd0def307ee48b76668
claudio-py <claudio.3777@gmail.com>
2025-06-22 23:51:25 -0300

    finalizado

--- CLang/prof_Renato/lista3/pedagio.c
+++ CLang/prof_Renato/lista3/pedagio.c
@@ -2,22 +2,25 @@
 
 int main() {
   char tipo;
-  prinf("aperte enter para iniciar\n");
+  printf("aperte enter para iniciar\n");
+    printf("ou pressione Ctrl+D para sair:\n");
+
   while ((tipo = getchar()) != EOF) {
     printf("digite qual o tipo de veiculo:\n");
     printf("A - moto\n");
     printf("B - carro\n");
     printf("C - caminhão\n");
-    if (tipo == 'A') {
+
+    if (tipo == 'A')
       printf("O valor do pedágio é R$ 2,00\n");
-    } else if (tipo == 'B') {
+
+    if (tipo == 'B')
       printf("O valor do pedágio é R$ 3,50\n");
-    } else if (tipo == 'C') {
+
+    if (tipo == 'C')
       printf("O valor do pedágio é R$ 5,00\n");
-    } else {
-      printf("Tipo de veículo inválido.\n");
-    }
-    printf("Digite outro tipo de veículo ou pressione Ctrl+D para sair:\n");
+
   }
+
   return 0;
 }
