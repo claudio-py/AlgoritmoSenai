2fc5e236000c14a247740cebc040b1d67c7a8d7c
claudio-py <claudio.3777@gmail.com>
2025-06-22 23:32:35 -0300

    update

--- CLang/prof_Renato/lista3/cpfVal.c
+++ CLang/prof_Renato/lista3/cpfVal.c
@@ -1,28 +1,28 @@
-
 #include <ctype.h>
 #include <stdio.h>
 
 int main() {
   char senha[30];
-  int i = 0, tem_num = 0, tem_letra = 0;
+  int i = 0;
 
   printf("Digite um cpf valido com 11 digitos, sem espaco e sem tracos:\n");
   scanf("%[^\n]s", senha);
 
   while (senha[i]) {
-    if (isdigit(senha[i])) {
-      tem_num = 1;
-    }
     if (isalpha(senha[i])) {
-      tem_letra = 1;
+      printf("CPF invalido, nao pode conter letras.\n");
+      return 0;
     }
+    if (i > 11) {
+      printf("CPF invalido, deve conter exatamente 11 digitos.\n");
+      return 0;
+    }
+
     i++;
   }
-  if (tem_letra) {
-    printf("CPF invalido, nao pode conter letras.\n");
-  }else if (tem_num && i == 11) {
-    printf("CPF valido.\n");
-  } else {
-    printf("CPF invalido, deve conter exatamente 11 digitos.\n");
-  }
+
+  i == 11 ? printf("CPF valido.\n")
+          : printf("CPF invalido, deve conter exatamente 11 digitos.\n");
+
+  return 0;
 }
