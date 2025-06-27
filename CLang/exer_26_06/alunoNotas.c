c2ad817c64c5e6a9193fe88ad1f60214be121a68
claudio-py <claudio.3777@gmail.com>
2025-06-27 17:13:26 -0300

    updates

--- CLang/exer_26_06/alunoNotas.c
+++ CLang/exer_26_06/alunoNotas.c
@@ -22,30 +22,30 @@ int main() {
   // alocacao dinamica com base no memory block size do tipo Aluno
   Aluno *alunos = malloc(numAlunos * sizeof(Aluno));
   if (!alunos) {
-    perror("malloc alunos");//usando a stdio.h para checar se alunos ta NULL
+    perror("malloc alunos"); // usando a stdio.h para checar se alunos ta NULL
     return 1;
   }
 
   for (int i = 0; i < numAlunos; i++) {
     // alocacao de memoria estatica apenas para carregar os dados
-    // que serao alocados na matriz dinamica, 
+    // que serao alocados na matriz dinamica,
     // ou seja, criei um vetor intermediario.
     char buffer[MAXCHAR];
 
     printf("\nNome do aluno %d: ", i + 1);
     scanf(" %[^\n]", buffer);
 
-//preparando o terrendo: alocando dinamicamente o espaco em memoria especifico 
-//para o tamanho do buffer intermediario que foi criado
+    // preparando o terrendo: alocando dinamicamente o espaco em memoria
+    // especifico para o tamanho do buffer intermediario que foi criado
     alunos[i].name = malloc(strlen(buffer) + 1);
 
     if (!alunos[i].name) {
       perror("malloc name");
       return 1;
     }
+    // finalmente o buffer entrega os dados e completa mais um ciclo de vida
     strcpy(alunos[i].name, buffer);
 
-    // Allocate grades
     alunos[i].grades = malloc(numNotas * sizeof(float));
     if (!alunos[i].grades) {
       perror("malloc grades");
@@ -58,7 +58,6 @@ int main() {
     }
   }
 
-  // Print everything
   printf("\nResumo:\n");
   for (int i = 0; i < numAlunos; i++) {
     printf("Aluno: %s\nNotas: ", alunos[i].name);
@@ -68,7 +67,6 @@ int main() {
     printf("\n");
   }
 
-  // Free memory
   for (int i = 0; i < numAlunos; i++) {
     free(alunos[i].name);
     free(alunos[i].grades);
