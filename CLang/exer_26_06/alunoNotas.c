794c0e11c5e41cca78303cca6e06d093aad3398f
claudio-py <claudio.3777@gmail.com>
2025-06-27 16:56:56 -0300

    updates

--- CLang/exer_26_06/alunoNotas.c
+++ CLang/exer_26_06/alunoNotas.c
@@ -1,45 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXCHAR 50 // numero maximo de caracteres
-#define NOTAS 5 // numero de notas lancadas 
-//vou terminar amanha!
+#include <string.h>
+
+#define MAXCHAR 50 // define uma constante em escopo global.
+
+// Struct for a student
+typedef struct {
+  char *name;
+  float *grades;
+} Aluno; // criei meu proprio tipo de dado porque nao ?
+
 int main() {
-  void **matriz;
-  int linha;
+  int numAlunos, numNotas;
 
-  printf("Quantos alunos deseja cadastrar?:\n");
-  scanf("%d", &linha);
-  getchar();
-  matriz = (void **)malloc(linha * sizeof(char *));
+  printf("Quantos alunos? ");
+  scanf("%d", &numAlunos);
 
-  if (matriz == NULL) {
-    printf("Erro de alocacao!\n");
+  printf("Quantas notas por aluno? ");
+  scanf("%d", &numNotas);
+
+  // alocacao dinamica com base no memory block size do tipo Aluno
+  Aluno *alunos = malloc(numAlunos * sizeof(Aluno));
+  if (!alunos) {
+    perror("malloc alunos");//usando a stdio.h para checar se alunos ta NULL
     return 1;
   }
 
-  for (int i = 0; i < linha; ++i) {
-    matriz[i] = (char *)malloc(MAXCHAR * sizeof(char));
-    if (matriz[i] == NULL) {
-      printf("Erro de alocacao!\n");
-      for (int k = 0; k < i; ++k)
-        free(matriz[k]);
-      free(matriz);
+  for (int i = 0; i < numAlunos; i++) {
+    // alocacao de memoria estatica apenas para carregar os dados
+    // que serao alocados na matriz dinamica, 
+    // ou seja, criei um vetor intermediario.
+    char buffer[MAXCHAR];
+
+    printf("\nNome do aluno %d: ", i + 1);
+    scanf(" %[^\n]", buffer);
+
+//preparando o terrendo: alocando dinamicamente o espaco em memoria especifico 
+//para o tamanho do buffer intermediario que foi criado
+    alunos[i].name = malloc(strlen(buffer) + 1);
+
+    if (!alunos[i].name) {
+      perror("malloc name");
       return 1;
+    }
+    strcpy(alunos[i].name, buffer);
+
+    // Allocate grades
+    alunos[i].grades = malloc(numNotas * sizeof(float));
+    if (!alunos[i].grades) {
+      perror("malloc grades");
+      return 1;
+    }
+
+    for (int j = 0; j < numNotas; j++) {
+      printf("Nota %d de %s: ", j + 1, alunos[i].name);
+      scanf("%f", &alunos[i].grades[j]);
     }
   }
 
-  for (int i = 0; i < linha; ++i) {
-    printf("aluno %d : ", i + 1);
-    fgets(matriz[i], MAXCHAR, stdin);
-    printf(" \n");
+  // Print everything
+  printf("\nResumo:\n");
+  for (int i = 0; i < numAlunos; i++) {
+    printf("Aluno: %s\nNotas: ", alunos[i].name);
+    for (int j = 0; j < numNotas; j++) {
+      printf("%.2f ", alunos[i].grades[j]);
+    }
+    printf("\n");
   }
 
-  for (int i = 0; i < linha; ++i) {
-    printf("%s\n", matriz[i]);
-    free(matriz[i]);
+  // Free memory
+  for (int i = 0; i < numAlunos; i++) {
+    free(alunos[i].name);
+    free(alunos[i].grades);
   }
-
-  free(matriz); // libera a memoria
+  free(alunos);
 
   return 0;
 }
